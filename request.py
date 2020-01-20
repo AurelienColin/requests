d46a1003f6c8b8398c009c0e7cf8f1901ce34933
Rignak <aureliencolin@hotmail.com>
2020-01-20 22:35:45 +0100

    Add browser cookies for requesting (enable log and higher request ratefor some website) .

--- request.py
+++ request.py
@@ -4,23 +4,24 @@ import bs4 as BeautifulSoup
 import time
 import os
 import shutil
+import browsercookie
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko)'
                          ' Chrome/23.0.1271.64 Safari/537.11'}
 RETRY_COUNT = 5
+cj = browsercookie.chrome()
 
 
 def request_with_retry(url, retry=RETRY_COUNT, soup=True, headers=HEADERS):
     ok = 0
     while ok < retry:
         try:
-            req = urllib.request.Request(url, headers=headers)
-            res = urllib.request.urlopen(req)
+            res = requests.get(url, headers=headers, cookies=cj)
             if soup:
-                res = BeautifulSoup.BeautifulSoup(res, "lxml")
+                res = BeautifulSoup.BeautifulSoup(res.text, "lxml")
             return res
         except Exception as e:
-            print(f'Error: {url} because of "{e}"')
+            print(f'request.request_with_retry: Error: {url} because of "{e}"')
             time.sleep(10)
             ok += 1
 
@@ -28,7 +29,7 @@ def request_with_retry(url, retry=RETRY_COUNT, soup=True, headers=HEADERS):
 def request_stream(url, payload={}, headers=HEADERS, retry=RETRY_COUNT):
     ok = 0
     while ok < retry:
-        r = requests.get(url, stream=True, data=payload, headers=headers)
+        r = requests.get(url, stream=True, data=payload, headers=headers, cookies=cj)
         if r.status_code == 200:
             return r.raw
         elif r.status_code == 429:  # spam
